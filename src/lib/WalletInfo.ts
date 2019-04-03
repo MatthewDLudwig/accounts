572b845aa5acd2ff48b25e817c0414a4b076dfd1
sisou <hello@soerenschwert.de>
2019-04-03 11:08:00 +0200

    Use Account[] type for list and migration results

--- src/lib/WalletInfo.ts
+++ src/lib/WalletInfo.ts
@@ -5,6 +5,7 @@ import {
     HashedTimeLockedContractInfo,
     ContractInfoEntry,
 } from './ContractInfo';
+import { Account } from './PublicRequestTypes';
 
 export enum WalletType {
     LEGACY = 1,
@@ -74,6 +75,18 @@ export class WalletInfo {
             keyMissing: this.keyMissing,
             fileExported: this.fileExported,
             wordsExported: this.wordsExported,
+        };
+    }
+
+    public toAccountType(): Account {
+        return {
+            accountId: this.id,
+            label: this.label,
+            type: this.type,
+            fileExported: this.fileExported,
+            wordsExported: this.wordsExported,
+            addresses: Array.from(this.accounts.values()).map((address) => address.toAddressType()),
+            contracts: this.contracts.map((contract) => contract.toContractType()),
         };
     }
 }
